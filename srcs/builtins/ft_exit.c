5b711f7d1e589c676c1bff7be2c4c47e2879db64
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-08-17 17:43:06 +0200

    exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 13:19:43 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/16 10:32:28 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/17 15:48:41 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,48 @@
 
 static bool	check_exit_token(char *arg)
 {
-	while (*arg)
+	if (!ft_isdigit(ft_atoi(arg)))
 	{
-		if (!ft_isdigit(*arg))
-		{
-			ft_printf("minishell: exit: %s: numeric argument required\n", *arg);
-			return (false);
-		}
-		arg++;
+		if (ft_strcmp(arg, "+") || ft_strcmp(arg, "--"))
+			return (true);
+		ft_printf("minishell: exit: %s: numeric argument required\n", arg);
+		return (false);
 	}
 	return (true);
 }
 
 void	ft_exit(t_global *global)
 {
+	int	i;
+
+	i = 1;
 	global->exit_code = 0;
 	ft_printf("exit\n");
-	if (!global->line->token[1])
-		exit(global->exit_code);
-	if (!check_exit_token(global->line->token[1]))
+	if (ft_strcmp(*global->line->token, "--"))
+		i++;
+	if (!global->line->token[i])
+		exit (global->exit_code);
+	else if (!check_exit_token(global->line->token[i]))
 		global->exit_code = 255;
-	else if (global->line->token[2])
+	else if (global->line->token[i + 1])
 	{
 		ft_printf("minishell: exit: too many arguments\n");
 		return ;
 	}
-	else if (global->line->token[1])
+	else if (global->line->token[i])
 	{
-		global->exit_code = ft_atoi(global->line->token[1]);
+		// if (!ft_strcmp(global->line->token[1], "--"))
+		// {
+		// 	if (global->line->token[i + 1])
+		// 	{
+		// 		i++;
+		// 		if (!check_exit_token(global->line->token[i]))
+		// 			global->exit_code = 255;
+		// 	}
+		// }
+		global->exit_code = ft_atoi(global->line->token[i]);
 		if (global->exit_code < 0 || global->exit_code > 255)
 			global->exit_code %= 256;
 	}
-	exit(global->exit_code);
+	exit (global->exit_code);
 }
