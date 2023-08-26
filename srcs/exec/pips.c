013147bf584841348d79538a1e3328427db84cc7
Maria <mv9312550@gmail.com>
2023-08-26 10:54:28 +0200

    💻 Auto-commit

--- srcs/exec/pips.c
+++ srcs/exec/pips.c
@@ -6,7 +6,7 @@
 /*   By: mariavillarroel <mariavillarroel@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 17:29:45 by mvillarr          #+#    #+#             */
-/*   Updated: 2023/08/25 12:24:04 by mariavillar      ###   ########.fr       */
+/*   Updated: 2023/08/26 10:52:02 by mariavillar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 // filename path : global->line->cmds->redirection->filename
 
-char *ft_pipe(t_global *global, char *paths)
-{
-    int parent_process;
+// char *ft_pipe(t_global *global, char *paths)
+// {
+//     int parent_process;
+//     int pid;
     
-    
-    paths = global->line->cmds->input;
-    check_pipe(char **av, );
-}
+//     paths = global->line->cmds->input;
+//     check_pipe(char **av, );
+// }
 
-check_pipe(char **av, )
-{
+// check_pipe(char **av, )
+// {
     
-}
+// }
