b7bfaa83199da70c6fc920a484eb693931233f4e
Raphael Rouiller <rrouille@c1r7s8.42lausanne.ch>
2023-08-26 16:48:44 +0200

    💻 Auto-commit

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 07:34:38 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/25 19:00:54 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/26 16:46:49 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,99 +37,95 @@ static void	echo_print(char *str)
 	}
 }
 
-// static char **split_cmd_to_tokens(char *cmd)
-// {
-// 	int count = 0;
-// 	char **tokens;
-// 	char *token;
-// 	char *cmd_copy = ft_strdup(cmd);
+static char **split_cmd_to_tokens(char *cmd)
+{
+	int count = 0;
+	char **tokens;
+	char *token;
+	char *cmd_copy = ft_strdup(cmd);
 
-// 	token = ft_strtok(cmd_copy, " ");
-// 	while (token)
-// 	{
-// 		count++;
-// 		token = ft_strtok(NULL, " ");
-// 	}
-// 	cmd_copy = ft_strdup(cmd);
-// 	tokens = (char **)ft_gc_malloc((count + 1) * sizeof(char *));
-// 	count = 0;
-// 	token = ft_strtok(cmd_copy, " ");
-// 	while (token)
-// 	{
-// 		tokens[count] = ft_strdup(token);
-// 		count++;
-// 		token = ft_strtok(NULL, " ");
-// 	}
-// 	tokens[count] = NULL;
-// 	return tokens;
-// }
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char **split_cmd_to_tokens(char *cmd) {
-    int count = 0;
-    char **tokens;
-    char *start = cmd;
-    char *token;
-    int in_quotes = 0;
-
-    // First pass: Count tokens
-    while (*start) {
-        while (*start == ' ') start++; // skip spaces
-
-        if (*start == '\'') {
-            in_quotes = !in_quotes; // toggle quote state
-            start++; // move to next char
-        }
-
-        if (!in_quotes) {
-            while (*start && *start != ' ') start++;
-        } else {
-            while (*start && *start != '\'') start++;
-            if (*start == '\'') {
-                in_quotes = 0; // exit quote state
-                start++; // move past the closing quote
-            }
-        }
-        count++;
-    }
-
-    tokens = (char **)malloc((count + 1) * sizeof(char *));
-    start = cmd;
-    count = 0;
-    in_quotes = 0;
-
-    // Second pass: Extract tokens
-    while (*start) {
-        while (*start == ' ') start++; // skip spaces
-
-        if (*start == '\'') {
-            in_quotes = !in_quotes;
-            token = start + 1; // start of token
-            start++; // move to next char
-        } else {
-            token = start; // start of token
-        }
-
-        if (!in_quotes) {
-            while (*start && *start != ' ') start++;
-        } else {
-            while (*start && *start != '\'') start++;
-            if (*start == '\'') {
-                in_quotes = 0; // exit quote state
-                start++; // move past the closing quote
-            }
-        }
-
-        tokens[count] = ft_strndup(token, start - token - (token[-1] == '\''));
-        count++;
-    }
-
-    tokens[count] = NULL;
-    return tokens;
+	token = ft_strtok(cmd_copy, " ");
+	while (token)
+	{
+		count++;
+		token = ft_strtok(NULL, " ");
+	}
+	cmd_copy = ft_strdup(cmd);
+	tokens = (char **)ft_gc_malloc((count + 1) * sizeof(char *));
+	count = 0;
+	token = ft_strtok(cmd_copy, " ");
+	while (token)
+	{
+		tokens[count] = ft_strdup(token);
+		count++;
+		token = ft_strtok(NULL, " ");
+	}
+	tokens[count] = NULL;
+	return tokens;
 }
+
+// char **split_cmd_to_tokens(char *cmd) {
+//     int count = 0;
+//     char **tokens;
+//     char *start = cmd;
+//     char *token;
+//     int in_quotes = 0;
+
+//     // First pass: Count tokens
+//     while (*start) {
+//         while (*start == ' ') start++; // skip spaces
+
+//         if (*start == '\'') {
+//             in_quotes = !in_quotes; // toggle quote state
+//             start++; // move to next char
+//         }
+
+//         if (!in_quotes) {
+//             while (*start && *start != ' ') start++;
+//         } else {
+//             while (*start && *start != '\'') start++;
+//             if (*start == '\'') {
+//                 in_quotes = 0; // exit quote state
+//                 start++; // move past the closing quote
+//             }
+//         }
+//         count++;
+//     }
+
+//     tokens = (char **)malloc((count + 1) * sizeof(char *));
+//     start = cmd;
+//     count = 0;
+//     in_quotes = 0;
+
+//     // Second pass: Extract tokens
+//     while (*start) {
+//         while (*start == ' ') start++; // skip spaces
+
+//         if (*start == '\'') {
+//             in_quotes = !in_quotes;
+//             token = start + 1; // start of token
+//             start++; // move to next char
+//         } else {
+//             token = start; // start of token
+//         }
+
+//         if (!in_quotes) {
+//             while (*start && *start != ' ') start++;
+//         } else {
+//             while (*start && *start != '\'') start++;
+//             if (*start == '\'') {
+//                 in_quotes = 0; // exit quote state
+//                 start++; // move past the closing quote
+//             }
+//         }
+
+//         tokens[count] = ft_strndup(token, start - token - (token[-1] == '\''));
+//         count++;
+//     }
+
+//     tokens[count] = NULL;
+//     return tokens;
+// }
 
 void	ft_echo(char *cmd)
 {
