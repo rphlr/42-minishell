4c474121c1155bc0cbb1800b2b20d1b770086f6c
Raphael <rrouille@student.42lausanne.ch>
2023-08-26 09:20:24 +0200

    Pipe nearly working

--- srcs/exec/execution.c
+++ srcs/exec/execution.c
@@ -6,7 +6,7 @@
 /*   By: rrouille <rrouille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 16:57:29 by rrouille          #+#    #+#             */
-/*   Updated: 2023/08/25 18:59:59 by rrouille         ###   ########.fr       */
+/*   Updated: 2023/08/26 09:19:45 by rrouille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,10 +81,7 @@ static void	execute_primaries(char	*cmd, t_global *global)
 	char *first_word = ft_strtok(cmd_copy, " ");
 
 	if (!ft_strcmp(first_word, "echo"))
-	{
-		printf("cmd = %s\n", cmd);
 		ft_echo(cmd);
-	}
 	else if (!ft_strcmp(first_word, "cd"))
 		ft_cd(cmd, global);
 	else if (!ft_strcmp(first_word, "pwd"))
@@ -174,6 +171,9 @@ static int execute_cmd(char *cmd, t_redirection *redir, t_global *global)
 					break;
 			}
 		}
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		// tcsetattr(0, TCSANOW, &original_term);
 		execve(path, argv, NULL);
 		global->exit_code = EXIT_FAILURE;
 		exit (manage_exit(&global->exit_code));
@@ -211,35 +211,221 @@ static void	ft_and(t_global *global, t_cmds *curr_cmd, t_cmds *next_cmd)
 	}
 }
 
-static void	ft_pipe(t_global *global, t_cmds *curr_cmd, t_cmds *next_cmd)
+// static void execute_pipeline(t_global *global, t_cmds *cmds)
+// {
+//     int num_cmds = 0;
+//     t_cmds *tmp = cmds;
+//     while (tmp)
+//     {
+//         num_cmds++;
+//         tmp = tmp->next;
+//     }
+
+//     int fds[num_cmds - 1][2];
+//     for (int i = 0; i < num_cmds - 1; i++)
+//     {
+//         pipe(fds[i]);
+//     }
+
+//     for (int i = 0; i < num_cmds; i++)
+//     {
+//         pid_t pid = fork();
+//         if (pid == 0)
+//         {
+//             // Si ce n'est pas la première commande, redirigez l'entrée standard depuis le pipe précédent
+//             if (i != 0)
+//             {
+//                 dup2(fds[i - 1][0], STDIN_FILENO);
+//             }
+
+//             // Si ce n'est pas la dernière commande, redirigez la sortie standard vers le pipe actuel
+//             if (i != num_cmds - 1)
+//             {
+//                 dup2(fds[i][1], STDOUT_FILENO);
+//             }
+
+//             // Fermez tous les descripteurs de fichiers
+//             for (int j = 0; j < num_cmds - 1; j++)
+//             {
+//                 close(fds[j][0]);
+//                 close(fds[j][1]);
+//             }
+
+//             // Restaurez le comportement par défaut des signaux
+//             signal(SIGINT, SIG_DFL);
+//             signal(SIGQUIT, SIG_DFL);
+
+//             execute_cmd(cmds->cmd, cmds->redir, global);
+//             exit(EXIT_SUCCESS);
+//         }
+
+//         cmds = cmds->next;
+//     }
+
+//     // Fermez tous les descripteurs de fichiers dans le processus parent
+//     for (int i = 0; i < num_cmds - 1; i++)
+//     {
+//         close(fds[i][0]);
+//         close(fds[i][1]);
+//     }
+
+//     // Attendez que tous les processus enfants se terminent
+//     for (int i = 0; i < num_cmds; i++)
+//     {
+//         wait(NULL);
+//     }
+// }
+
+static void execute_pipeline(t_global *global, t_cmds *cmds)
+{
+    int num_cmds = 0;
+    t_cmds *tmp = cmds;
+    while (tmp)
+    {
+        num_cmds++;
+        tmp = tmp->next;
+    }
+    int fds[num_cmds - 1][2];
+    for (int i = 0; i < num_cmds - 1; i++)
+    {
+        if (pipe(fds[i]) < 0)
+        {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < num_cmds; i++)
+    {
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            if (i != 0) // Si ce n'est pas la première commande
+            {
+                dup2(fds[i - 1][0], STDIN_FILENO);
+                close(fds[i - 1][0]);
+            }
+            if (i != num_cmds - 1) // Si ce n'est pas la dernière commande
+            {
+                dup2(fds[i][1], STDOUT_FILENO);
+                close(fds[i][1]);
+            }
+            
+            // Fermeture de tous les descripteurs de fichier inutilisés
+            for (int j = 0; j < num_cmds - 1; j++)
+            {
+                if (j != i - 1) close(fds[j][0]);
+                if (j != i) close(fds[j][1]);
+            }
+            signal(SIGINT, SIG_DFL);
+            signal(SIGQUIT, SIG_DFL);
+
+            execute_cmd(cmds->cmd, cmds->redir, global);
+            exit(EXIT_SUCCESS);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        cmds = cmds->next;
+    }
+    
+    // Fermeture de tous les descripteurs de fichier dans le processus parent
+    for (int i = 0; i < num_cmds - 1; i++)
+    {
+        close(fds[i][0]);
+        close(fds[i][1]);
+    }
+    
+    // Attendre que tous les processus fils se terminent
+    for (int i = 0; i < num_cmds; i++)
+    {
+        wait(NULL);
+    }
+}
+
+
+void	ft_pipe(t_global *global, t_cmds *curr_cmd, t_cmds *next_cmd)
 {
 	int		fd[2];
-	pid_t	pid;
+	pid_t	pid1, pid2;
 
 	pipe(fd);
-	pid = fork();
-	if (pid == 0)
+	pid1 = fork();
+	if (pid1 == 0)
 	{
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[0]);
 		close(fd[1]);
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
 		execute_cmd(curr_cmd->cmd, curr_cmd->redir, global);
+		exit(EXIT_SUCCESS);
 	}
-	else if (pid < 0)
+	else if (pid1 < 0)
 	{
 		ft_printf("minishell: fork error: %s\n", strerror(errno));
 		global->exit_code = 1;
 		manage_exit(&global->exit_code);
 		return ;
 	}
-	else
+
+	pid2 = fork();
+	if (pid2 == 0)
 	{
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
 		close(fd[1]);
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
 		execute_cmd(next_cmd->cmd, next_cmd->redir, global);
+		exit(EXIT_SUCCESS);
 	}
+	else if (pid2 < 0)
+	{
+		ft_printf("minishell: fork error: %s\n", strerror(errno));
+		global->exit_code = 1;
+		manage_exit(&global->exit_code);
+		return ;
+	}
+
+	close(fd[0]);
+	close(fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
 }
+
+// static void	ft_pipe(t_global *global, t_cmds *curr_cmd, t_cmds *next_cmd)
+// {
+// 	int		fd[2];
+// 	pid_t	pid;
+
+// 	pipe(fd);
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		dup2(fd[1], STDOUT_FILENO);
+// 		close(fd[0]);
+// 		close(fd[1]);
+// 		execute_cmd(curr_cmd->cmd, curr_cmd->redir, global);
+// 	}
+// 	else if (pid < 0)
+// 	{
+// 		ft_printf("minishell: fork error: %s\n", strerror(errno));
+// 		global->exit_code = 1;
+// 		manage_exit(&global->exit_code);
+// 		return ;
+// 	}
+// 	else
+// 	{
+// 		dup2(fd[0], STDIN_FILENO);
+// 		close(fd[0]);
+// 		close(fd[1]);
+// 		execute_cmd(next_cmd->cmd, next_cmd->redir, global);
+// 	}
+// }
 
 static void	ft_semicolon(t_global *global, t_cmds *curr_cmd, t_cmds *next_cmd)
 {
@@ -292,7 +478,69 @@ static void	execute_specials(t_global *global)
 		else if (count_tmp->nbr_pipes > 0 && *type_tmp == PIPE)
 		{
 			count_tmp->nbr_pipes--;
-			ft_pipe(global, curr_cmd, curr_cmd->next);
+			// ft_pipe(global, curr_cmd, curr_cmd->next);
+			// int num_pipes = 0;
+			// t_cmds *tmp_cmd = curr_cmd;
+			// count_tmp->nbr_pipes--;
+			// execute_pipeline(global, curr_cmd);
+
+			// Avancez jusqu'à la fin du pipeline
+
+			// t_token	*tmp = type_tmp;
+			// while (*type_tmp != PIPE)
+			// {
+			// 	curr_cmd = curr_cmd->next;
+			// 	type_tmp++;
+			// }
+			// t_token	*tmp_type = type_tmp;
+			// while ((*type_tmp == PIPE) || !((*type_tmp == INPUT || *type_tmp == OUTPUT || *type_tmp == APPEND || *type_tmp == HEREDOC || *type_tmp == OR || *type_tmp == AND || *type_tmp == SEMICOLON) && *type_tmp != END))
+			// {
+			// 	if (*type_tmp == PIPE)
+			// 	{
+			// 		num_pipes++;
+			// 	}
+			// 	type_tmp++;
+			// }
+			// printf("num_pipes = %d\n", num_pipes);
+			// // type_tmp = tmp_type;
+			// // Move to the last command in the pipeline
+			// for (int i = 0; i <= num_pipes; i++)
+			// {
+			// 	printf("curr_cmd->cmd = %s\n", curr_cmd->cmd);
+			// 	printf("i = %d\n", i);
+			// 	tmp_cmd = tmp_cmd->next;
+			// }
+			
+			// execute_pipeline(global, curr_cmd);
+			// curr_cmd = tmp_cmd;
+			// int num_pipes = 0;
+			// t_cmds *tmp_cmd = curr_cmd;
+
+			// while (*type_tmp == PIPE)
+			// {
+			// 	num_pipes++;
+			// 	type_tmp++;
+			// }
+
+			// Move type_tmp until next special char or end
+			while (!(*type_tmp == INPUT || *type_tmp == OUTPUT || *type_tmp == APPEND ||
+					*type_tmp == HEREDOC || *type_tmp == OR || *type_tmp == AND ||
+					*type_tmp == SEMICOLON || *type_tmp == END) || *type_tmp == PIPE)
+			{
+				if (*type_tmp == PIPE)
+					count_tmp->nbr_pipes--;
+				type_tmp++;
+			}
+
+			// // Move to the last command in the pipeline
+			// for (int i = 0; i < num_pipes; i++)
+			// {
+			// 	if (tmp_cmd)
+			// 		tmp_cmd = tmp_cmd->next;
+			// }
+			printf("curr_cmd->cmd = %s\n", curr_cmd->cmd);
+			execute_pipeline(global, curr_cmd);
+			// curr_cmd = tmp_cmd;
 		}
 		curr_cmd = curr_cmd->next;
 		type_tmp++;
@@ -308,7 +556,6 @@ void	run_cmd(t_global *global)
 {
 	int primaries;
 
-	printf("cmd = %s\n", global->line->cmds->cmd);
 	primaries = cmd_is_primaries(global->line->cmds->cmd);
 	if (primaries)
 	{
